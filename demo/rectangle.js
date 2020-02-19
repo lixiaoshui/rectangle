6c57324907723712fa2ebff4b033fac3e36d8113
lixiaoshui <2087751927@qq.com>
2020-02-19 10:07:30 +0800

    twice

--- demo/rectangle.js
+++ demo/rectangle.js
@@ -6,18 +6,20 @@ $(function(){
     var $btnCal = $('#calculate');
     var $perimeter = $('#perimeter');
     var $area = $('#area');
+
     $btnCal.click(function(){
         //get value
         var w = Number($width.val());
         var h = Number($height.val());
         //calculate
-        var p=2*w+2*h;
-        var a=w*h;
+        var p=roundFractional(2*w+2*h,2);
+        var a=roundFractional(w*h,2);
         //output
-        $perimeter.roundFractional(p);
-        $area.roundFractional(a);
+        $perimeter.val(p);
+        $area.val(a);
     })
+    
 })
-function roundFractional(x,n){
+function roundFractional(x, n) {
     return Math.round(x * Math.pow(10, n)) / Math.pow(10, n);
 }
